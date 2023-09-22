37c769826603053a4cb5594fe7c3d86ce7c921e4
Seren_Modz 21 <seren@kings-world.net>
2023-09-22 01:54:23 +0100

    fix: 12-hour clock displaying 00 for the hour
    Signed-off-by: Seren_Modz 21 <seren@kings-world.net>

--- packages/core/src/util.ts
+++ packages/core/src/util.ts
@@ -7,7 +7,7 @@ export const handleTimestamp = (value: DiscordTimestamp, hour24 = false): string
 		throw new TypeError('Timestamp prop must be a Date object or a string.');
 	}
 
-	return new Date(value)
+	const date = new Date(value)
 		.toLocaleDateString(undefined, {
 			year: 'numeric',
 			month: '2-digit',
@@ -17,6 +17,8 @@ export const handleTimestamp = (value: DiscordTimestamp, hour24 = false): string
 			hour12: !hour24
 		})
 		.replace(',', '');
+
+	return hour24 ? date : date.replace(/00:(\d{2})/, '12:$1');
 };
 
 export const getGlobalEmojiUrl = (emojiName: string): Emoji | undefined => window.$discordMessage?.emojis?.[emojiName];
